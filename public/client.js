4ffa02378b22778ff5ecad8fcabcd8fa7bd4e57c
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2020-05-24 14:04:13 -0400

    add gitignore

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-console.log("hello world :o");
+console.log("hello world!!!");
 const dreams = [];
 
 // define variables that reference elements on our page
